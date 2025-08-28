c690f252e11c4662d7658ff2112a4d5659de2678
JINI093 <JINI093@users.noreply.github.com>
2025-08-28 07:53:16 +0000

    deploy: f1849f0d102ba185cfe5abcfcd0c3039ca926fd8

--- admin/flutter_bootstrap.js
+++ admin/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2709478851"
+    serviceWorkerVersion: "505654622"
   }
 });
